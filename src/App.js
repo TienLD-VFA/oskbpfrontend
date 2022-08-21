51ed52d683916c23e09e169c657348506d1fa36e
Pham Anh Dung <anhdungpham090@gmail.com>
2022-08-21 21:34:25 +0700

    refactor code in memory component and app component.

--- src/App.js
+++ src/App.js
@@ -7,28 +7,31 @@ import "framework7/css/bundle";
 Framework7.use(Framework7React);
 // sase
 function App() {
-  const [step, setStep] = useState(0);
-  const _HandleStep = () => {
-    setStep(step + 1);
-  };
-  const _HandleStepPrev = () => {
-    setStep(step - 1);
-  };
-  return (
-    //
-    <div className="App">
-      <Memory x={step} />
-      <div className="button">
-        <button className="button__next" onClick={_HandleStep}>
-          NEXT
-        </button>
-        <button className="button_next" onClick={_HandleStepPrev}>
-          Prev
-        </button>
-      </div>
-      <div className="button__text">{step}</div>
-    </div>
-  );
+	const [step, setStep] = useState(0);
+
+	// shouldn't there be a check to disable _HandleStep and _HandleStepPrev if the step is out of bound?
+	const _HandleStep = () => {
+		setStep((s) => (s < 7 ? s + 1 : s));
+	};
+	const _HandleStepPrev = () => {
+		setStep((s) => (s > 0 ? s - 1 : s));
+	};
+
+	return (
+		//
+		<div className="App">
+			<Memory x={step} />
+			<div className="button">
+				<button className="button__next" onClick={_HandleStep} disabled={step === 7}>
+					NEXT
+				</button>
+				<button className="button_next" onClick={_HandleStepPrev} disabled={step === 0}>
+					Prev
+				</button>
+			</div>
+			<div className="button__text">{step}</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Memory/Memory.jsx
+++ src/components/Memory/Memory.jsx
@@ -4,117 +4,99 @@
  * Created by tienld@vitalify.asia on 26/07/2022
  * Copyright (c) 2022 OMRON HEALTHCARE Co.,Ltd. All rights reserved.
  */
+
+// NOTE: Move style import to the top of the file
+import "./Memory.scss";
 import React, { useMemo } from "react";
 import { Progressbar } from "framework7-react";
 import PropTypes from "prop-types";
-import "./Memory.scss";
+import {
+	PERCENTAGE_OF_PROGRESS_BAR,
+	COMPARE_STEPS,
+	COLORS,
+	TOTAL_STEPS,
+} from "./constant";
 
-const Memory = ({ x }) => {
-  /* A constant variable that is used to set the color of the progressbar. */
-  const COLORS = {
-    active: "#005EB8",
-    completed: "#335A75",
-    default: "#C3CACF",
-  };
+const Memory = ({ x = 0 }) => {
+	// NOTE: Pls create a new line between variables like i did for better readability
 
-  /* Used to compare the value of the progressbar. */
-  const COMPARE_STEPS = {
-    COMPARE_1: 0,
-    COMPARE_2: 2,
-    COMPARE_3: 4,
-    COMPARE_4: 6,
-    COMPARE_5: 7,
-  };
+	const PERCENT = useMemo(() => {
+		const REAL_X = x + 1;
+		const value = PERCENTAGE_OF_PROGRESS_BAR[`STEP_${REAL_X}`];
 
-  /* Used to represent the percentage of the progressbar increasing gradually. */
-  const PERCENTAGE_OF_PROGREESBAR = {
-    STEP_1: 0.15,
-    STEP_2: 2.15,
-    STEP_3: 4.3,
-    STEP_4: 6.3,
-    STEP_5: 8.5,
-    STEP_6: 9.5,
-    STEP_7: 11,
-    STEP_8: 12.5,
-  };
+		return 100 * (TOTAL_STEPS / 100) * value;
+	}, [x]);
 
-  const TOTAL_STEPS = 8;
-  const REAL_X = useMemo(() => x + 1, [x]);
-  const PERCENT = useMemo(() => {
-    const realX = REAL_X;
-    const value = PERCENTAGE_OF_PROGREESBAR[`STEP_${realX}`];
-    const result = 100 * (TOTAL_STEPS / 100) * value;
-    return result;
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [REAL_X]);
-  const LIST_BREAKPOINTS = useMemo(() => Object.values(COMPARE_STEPS), []);
-  const renderBreakpoints = () => {
-    return LIST_BREAKPOINTS.map((_, index) => {
-      const isLast = index >= LIST_BREAKPOINTS.length - 2;
-      const isStep3 = index === LIST_BREAKPOINTS.length - 3;
-      if (isLast) {
-        return null;
-      } else {
-        const nextBreakPointValue = LIST_BREAKPOINTS[index + 1];
-        const currentBreakPointValue = LIST_BREAKPOINTS[index];
-        const isActive = x >= currentBreakPointValue;
-        const isComplete = !isStep3 && x >= nextBreakPointValue;
-        const realIndex = index + 1;
+	const renderBreakpoints = () => {
+		const LIST_BREAKPOINTS = Object.values(COMPARE_STEPS);
 
-        return (
-          <div className="memory_label_wrap">
-            <div
-              className="memory_label_wrap_bar"
-              style={{
-                background: isComplete
-                  ? COLORS.completed
-                  : isActive
-                  ? COLORS.active
-                  : COLORS.default,
-              }}
-            ></div>
-            <div
-              className="memory_label_wrap_title"
-              style={{
-                color: isComplete
-                  ? COLORS.completed
-                  : isActive
-                  ? COLORS.active
-                  : COLORS.default,
-              }}
-            >
-              Step {realIndex}
-            </div>
-          </div>
-        );
-      }
-    });
-  };
+		return LIST_BREAKPOINTS.map((_, index) => {
+			const isLast = index >= LIST_BREAKPOINTS.length - 2;
+			const isStep3 = index === LIST_BREAKPOINTS.length - 3;
 
-  return (
-    <div className="memory">
-      <div className="memory_label">
-        {x < COMPARE_STEPS.COMPARE_5 ? (
-          renderBreakpoints()
-        ) : (
-          <div
-            className="memory_label_wrap_complete"
-            style={{
-              color: COLORS.active,
-            }}
-          >
-            Step 4
-          </div>
-        )}
-      </div>
-      <div className="memory_progressbar">
-        <Progressbar progress={PERCENT} />
-      </div>
-    </div>
-  );
+			if (isLast) return null;
+
+			// NOTE: i don't think you have to create this much variable
+			// pls consider to write inline calculation.
+			const nextBreakPointValue = LIST_BREAKPOINTS[index + 1];
+			const currentBreakPointValue = LIST_BREAKPOINTS[index];
+			const isActive = x >= currentBreakPointValue;
+			const isComplete = !isStep3 && x >= nextBreakPointValue;
+			const realIndex = index + 1;
+
+      // NOTE: this one is the one you should create extra variable for
+      // it appear multiple times in file
+			const colorStyle = isComplete
+				? COLORS.completed
+				: isActive
+				? COLORS.active
+				: COLORS.default;
+
+			return (
+				<div className="memory_label_wrap">
+					<div
+						className="memory_label_wrap_bar"
+						style={{
+							backgroundColor: colorStyle,
+						}}
+					></div>
+					<div
+						className="memory_label_wrap_title"
+						style={{
+							color: colorStyle,
+						}}
+					>
+						Step {realIndex}
+					</div>
+				</div>
+			);
+		});
+	};
+
+	return (
+		<div className="memory">
+			<div className="memory_label">
+				{x < COMPARE_STEPS.COMPARE_5 ? (
+					renderBreakpoints()
+				) : (
+					<div
+						className="memory_label_wrap_complete"
+						style={{
+							color: COLORS.active,
+						}}
+					>
+						Step 4
+					</div>
+				)}
+			</div>
+			<div className="memory_progressbar">
+				<Progressbar progress={PERCENT} />
+			</div>
+		</div>
+	);
 };
 export default Memory;
 
 Memory.propTypes = {
-  x: PropTypes.number,
+	x: PropTypes.number,
 };
