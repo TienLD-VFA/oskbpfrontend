d243e185ad2d7f132231c810b39967576c447256
Pham Anh Dung <anhdungpham090@gmail.com>
2022-09-17 00:46:28 +0700

    Carousel example.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./App.scss";
 import Memory from "./components/Memory/Memory";
 import Framework7 from "framework7/lite-bundle";
-import Framework7React from "framework7-react";
+import Framework7React, { Swiper } from "framework7-react";
 import "framework7/css/bundle";
 Framework7.use(Framework7React);
 // sase
